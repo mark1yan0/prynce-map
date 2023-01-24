291c7c2c61fdb27e055e0104c61bf1f39b0cee18
Mark1yan1 <markkmit@gmail.com>
2023-01-24 18:23:31 +0100

    solves mocks typing errors

--- src/lib/mock/index.ts
+++ src/lib/mock/index.ts
@@ -7,6 +7,7 @@ export const mockItems: IMapItem[] = [
     address: 'Via Chanoux, 123 - 11026 Saint Vincent (AO)',
     lat: 45.7510001,
     lon: 7.6454691,
+    slug: 'charme_arredamenti',
     link: 'https://www.google.com/maps/place/Via+Emile+Chanoux,+123,+11027+Saint-Vincent+AO/@45.7510001,7.6454691,21z/data=!4m5!3m4!1s0x4788c6e57db59e61:0x8fad28c8bcff2185!8m2!3d45.7509756!4d7.6453947',
   },
   {
@@ -15,6 +16,7 @@ export const mockItems: IMapItem[] = [
     address: 'Piazza Roma, 54 - 25016 Ghedi (BS)',
     lat: 45.4020729,
     lon: 10.277689,
+    slug: 'gioielleria_godenzi',
     link: 'https://www.google.com/maps/place/Gioielleria+Godenzi/@45.4020746,10.2776888,15z/data=!4m2!3m1!1s0x0:0xfbbe74968b8d6e89?sa=X&ved=2ahUKEwidn__4pcP0AhVRg_0HHR5nBVQQ_BJ6BAhIEAU',
   },
   {
@@ -23,6 +25,7 @@ export const mockItems: IMapItem[] = [
     address: 'Corso Umberto I, 1, 28838 Stresa VB',
     lat: 45.8841227,
     lon: 8.5407552,
+    slug: 'gioielleria_paulon',
     link: 'https://www.google.com/maps/place/Gioielleria+Paulon+-+Luxury+Store/@45.8841227,8.5407552,18z/data=!3m1!4b1!4m5!3m4!1s0x4786758c178dd121:0x9ecc8a4316bfd39e!8m2!3d45.8841064!4d8.5407481',
   },
   {
@@ -31,6 +34,7 @@ export const mockItems: IMapItem[] = [
     address: 'Via Chanoux, 123 - 11026 Saint Vincent (AO)',
     lat: 45.7510001,
     lon: 7.6454691,
+    slug: 'charme_arrpwriqpsedamenti',
     link: 'https://www.google.com/maps/place/Via+Emile+Chanoux,+123,+11027+Saint-Vincent+AO/@45.7510001,7.6454691,21z/data=!4m5!3m4!1s0x4788c6e57db59e61:0x8fad28c8bcff2185!8m2!3d45.7509756!4d7.6453947',
   },
   {
@@ -39,6 +43,7 @@ export const mockItems: IMapItem[] = [
     address: 'Piazza Roma, 54 - 25016 Ghedi (BS)',
     lat: 45.4020729,
     lon: 10.277689,
+    slug: 'gioielleriawewra_arredamenti',
     link: 'https://www.google.com/maps/place/Gioielleria+Godenzi/@45.4020746,10.2776888,15z/data=!4m2!3m1!1s0x0:0xfbbe74968b8d6e89?sa=X&ved=2ahUKEwidn__4pcP0AhVRg_0HHR5nBVQQ_BJ6BAhIEAU',
   },
   {
@@ -47,6 +52,7 @@ export const mockItems: IMapItem[] = [
     address: 'Corso Umberto I, 1, 28838 Stresa VB',
     lat: 45.8841227,
     lon: 8.5407552,
+    slug: 'luxury_sasdalkjfstore',
     link: 'https://www.google.com/maps/place/Gioielleria+Paulon+-+Luxury+Store/@45.8841227,8.5407552,18z/data=!3m1!4b1!4m5!3m4!1s0x4786758c178dd121:0x9ecc8a4316bfd39e!8m2!3d45.8841064!4d8.5407481',
   },
   {
@@ -55,6 +61,7 @@ export const mockItems: IMapItem[] = [
     address: 'Via Chanoux, 123 - 11026 Saint Vincent (AO)',
     lat: 45.7510001,
     lon: 7.6454691,
+    slug: 'charme_asgdArredamenti',
     link: 'https://www.google.com/maps/place/Via+Emile+Chanoux,+123,+11027+Saint-Vincent+AO/@45.7510001,7.6454691,21z/data=!4m5!3m4!1s0x4788c6e57db59e61:0x8fad28c8bcff2185!8m2!3d45.7509756!4d7.6453947',
   },
   {
@@ -63,6 +70,7 @@ export const mockItems: IMapItem[] = [
     address: 'Piazza Roma, 54 - 25016 Ghedi (BS)',
     lat: 45.4020729,
     lon: 10.277689,
+    slug: 'charme_god<aòdfaenzi',
     link: 'https://www.google.com/maps/place/Gioielleria+Godenzi/@45.4020746,10.2776888,15z/data=!4m2!3m1!1s0x0:0xfbbe74968b8d6e89?sa=X&ved=2ahUKEwidn__4pcP0AhVRg_0HHR5nBVQQ_BJ6BAhIEAU',
   },
   {
@@ -71,6 +79,7 @@ export const mockItems: IMapItem[] = [
     address: 'Corso Umberto I, 1, 28838 Stresa VB',
     lat: 45.8841227,
     lon: 8.5407552,
+    slug: 'charme_asdlkfPaulon',
     link: 'https://www.google.com/maps/place/Gioielleria+Paulon+-+Luxury+Store/@45.8841227,8.5407552,18z/data=!3m1!4b1!4m5!3m4!1s0x4786758c178dd121:0x9ecc8a4316bfd39e!8m2!3d45.8841064!4d8.5407481',
   },
 ];
