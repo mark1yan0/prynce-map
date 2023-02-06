07cd271694239749e467f8a109f0979da9fdf583
Mark1yan1 <markkmit@gmail.com>
2023-02-07 00:07:41 +0100

    configures vite config to custom build file name

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/app.bundle.js`,
+        chunkFileNames: `assets/app.bundle.js`,
+        assetFileNames: `assets/app.bundle.[ext]`,
+      },
+    },
+  },
+});
